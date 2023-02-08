4b994bff9c0429c752b015060567170080ac9455
gLenczuk <lenczuk.general@gmail.com>
2023-02-08 20:30:02 +0100

    Add partials support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+    plugins: [handlebars({partialDirectory: resolve(__dirname, 'partials')})],
     base: '/vanilla-template/',
     build: {
         rollupOptions: {
