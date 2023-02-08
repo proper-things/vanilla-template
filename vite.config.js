892cb803eaef34adc6b15f101c9f6856688b2621
gLenczuk <lenczuk.general@gmail.com>
2023-02-08 19:06:02 +0100

    Add deploy script

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: '/vanilla-template/',
     build: {
         rollupOptions: {
             input: {
